b25a8a8546f829975060d26344655d6f997cdfea
huxwell <65736835+huxwell-src@users.noreply.github.com>
2023-03-13 14:06:06 +0000

    button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,8 +12,9 @@ const Header = () => {
             </span>
           </a>
           <a href="https://drive.google.com/file/d/1riIgWk7QfUOJOyQsJ2KI59lOp_ctovmB/view?usp=share_link">
-            <button className="btn btn-sm ">Download CV</button>
-            <i class="fa-solid fa-download"></i>
+            <button className="btn btn-sm ">
+              Download CV <i class="fa-solid fa-download mx-1 text-white"></i>
+            </button>
           </a>
         </div>
       </div>
